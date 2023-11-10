84741f1d3ecfa8c0ab392a99410347d26a38a2ec
Pratyush <pratyushojha059@gmail.com>
2023-11-10 18:44:56 +0545

    Added date part

--- src/pages/AddItem/AddItem.js
+++ src/pages/AddItem/AddItem.js
@@ -19,10 +19,10 @@ const AddItem = ({handleAddItem}) => {
             <label htmlFor="location">Lost location:</label>
             <input type="text" id='location' name='location' placeholder='Enter the location it was lost' className='form-element form-margin' required />
           </div>
-          {/* <div className='partition'>
+          <div className='partition'>
             <label htmlFor="lost_date">Date lost:</label>
             <input type="date" id='lost_date' name='lost_date'  className='form-element form-margin' required />
-          </div> */}
+          </div>
           <div className='partition'>
             <label htmlFor="ownerName">Name of the owner:</label>
             <input type="text" id='ownerName' name='ownerName' className='form-element form-margin' placeholder='Enter owners name' required />
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -7,10 +7,10 @@ export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
     headers: {
         "Content-Type": "application/json",
-        common: {
-            'Access-Control-Allow-Origin': 'http://localhost:3000'
-        }
     },
+        common: {
+            'Access-Control-Allow-Origin': 'http://localhost:3000',
+        },
 
-    // withCredentials: true
+    withCredentials: true
 })
